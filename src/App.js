d0df9314cc1c843a04fcaa0e8519f5503a13d197
irinakadushkina <kadushkina.exceedteam@yandex.ru>
2022-05-13 12:32:13 +0300

    add redirect to sign-up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import React from "react";
 
-export const App = () => (
+export default () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<CanvasPage />} />
+      <Route path="/canvas" element={<CanvasPage />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/log-in" />
     </Routes>
